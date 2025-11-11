d129f28a7fcca30be8d519ac151feed5962e3748
Cao Anh <nguyenmanhcaoanh9103@gmail.com>
2025-11-11 11:17:45 +0700

    config: created entities

---
+++ src/main/java/com/example/myschedule/entity/Schedule.java
@@ -0,0 +1,39 @@
+package com.example.myschedule.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "schedules")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
+public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "task", nullable = false, length = 100)
+    String task;
+
+    @Column(name = "note", length = 200)
+    String note;
+
+    @Column(name = "schedule_date", nullable = false)
+    LocalDate scheduleDate;
+
+    @Column(name = "start_time", nullable = false)
+    LocalTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    LocalTime endTime;
+
+}
