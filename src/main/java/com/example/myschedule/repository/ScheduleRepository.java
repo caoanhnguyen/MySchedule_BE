4056d86aa347c493af1cb53e15303fcb6a760004
Cao Anh <nguyenmanhcaoanh9103@gmail.com>
2025-11-13 09:08:11 +0700

    hotfix: sorted schedules by start time

--- src/main/java/com/example/myschedule/repository/ScheduleRepository.java
+++ src/main/java/com/example/myschedule/repository/ScheduleRepository.java
@@ -12,6 +12,9 @@ import java.util.List;
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
-    @Query("SELECT s.id, s.task, s.note, s.scheduleDate, s.startTime, s.endTime FROM Schedule s WHERE s.scheduleDate = :scheduleDate")
+    @Query("SELECT s.id, s.task, s.note, s.scheduleDate, s.startTime, s.endTime " +
+            "FROM Schedule s " +
+            "WHERE s.scheduleDate = :scheduleDate " +
+            "ORDER BY s.startTime ASC")
     List<Object[]> findAllByScheduleDate(@Param("scheduleDate") LocalDate scheduleDate);
 }
